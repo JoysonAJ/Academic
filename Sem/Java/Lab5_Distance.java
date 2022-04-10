c83d07f860b22e5f1c423b8679dc266a1c4f4518
JoysonAJ <97296190+JoysonAJ@users.noreply.github.com>
2022-04-10 12:51:13 +0530

    Add files via upload

---
+++ Sem/Java/Lab5_Distance.java
@@ -0,0 +1,42 @@
+class Distance {
+    int feet;
+    float inches;
+
+    Distance() {
+        feet = 0;
+        inches = 0;
+    }
+    Distance(int f, float i) {
+        feet = f;
+        inches = i;
+    }
+    void Display() {
+        if (inches >= 12) {
+            float temp = inches;
+            int ft = (int) inches / 12;
+
+            inches = temp % 12;
+            feet = feet + ft;
+        }
+        System.out.println("--------------------------");
+        System.out.println("Feet is :" + feet);
+        System.out.println("Inches are :" + inches);
+    }
+    Distance Add(Distance d1) {
+        Distance d = new Distance();
+        d.feet = feet + d1.feet;
+        d.inches = inches + d1.inches;
+        return d;
+    }
+}
+class Lab5_Distance {
+    public static void main(String[] args) {
+        Distance d1 = new Distance(12, 105.5f);
+        d1.Display();
+        Distance d2 = new Distance(8, 27.5f);
+        d2.Display();
+        Distance d3 = new Distance();
+        d3 = d2.Add(d1);
+        d3.Display();
+    }
+}
